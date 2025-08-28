c12eaf46bc50317cccf31b8e5dcf3f48b0ef14de
Francesco D'Auria <dauriafrancescomaria@gmail.com>
2025-08-28 17:49:30 +0200

    Add Pub/Sub item (schema) creation

---
+++ 0 - Setup/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "active-incline-456207-k7"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_pubsub_schema" "example" {
+  name = "example-schema"
+  type = "AVRO"
+  definition = "{\n  \"type\" : \"record\",\n  \"name\" : \"Avro\",\n  \"fields\" : [\n    {\n      \"name\" : \"StringField\",\n      \"type\" : \"string\"\n    },\n    {\n      \"name\" : \"IntField\",\n      \"type\" : \"int\"\n    }\n  ]\n}\n"
+}
